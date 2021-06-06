39b0e373a5df74c270859d0ba421739fb7be373e
ShugoYabuno <shugo.ybn@gmai.com>
2021-06-06 18:18:13 +0900

    update: eslint prettier最適化

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,14 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  extends: ['eslint:recommended', '@vue/typescript', 'prettier'],
+  plugins: ['@typescript-eslint'],
   // add your custom rules here
-  rules: {},
+  rules: {}
 }
